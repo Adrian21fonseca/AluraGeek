eda295ca668ae779ae509c8099388c45d849bd77
Adrian Fonseca <adrian2020fm@gmail.com>
2024-07-06 14:32:56 -0500

    first commit

--- js/mostrarCard.js
+++ js/mostrarCard.js
@@ -2,10 +2,10 @@ import { conexionAPI } from "./conexionAPI.js";
 
 const lista = document.querySelector("[data-lista]")
 
-function crearCard(nombre,precio,imagen){
-    const carta = document.querySelector("div");
-    lista.className = "card";
-    lista.innerHTML = `<img class="producto" src="${imagen}" alt="producto"> 
+function crearCard(nombre,precio,url){
+    const carta = document.querySelector("li");
+    carta.className = "card";
+    carta.innerHTML = `<img class="producto" src="${url}" alt="producto"> 
         <p class="nombre_producto">${nombre}</p>
         <p class="precio">${precio} <img src="img/Vector.svg" alt="basura"></p>
     `
@@ -15,6 +15,8 @@ function crearCard(nombre,precio,imagen){
 
 async function listarCarta(){
     const listaAPI = await conexionAPI.listarCards()
+
+    listaAPI.forEach(carta => lista.appendChild(crearCard(carta.nombre,carta.precio,carta.url)))
 } 
 
 listarCarta()
